002ebe1e7976c93a8f959afffa19526198c16a77
za-dengyongqing <dengyongqing@aliyun.com>
2019-01-08 13:44:12 +0800

    add throttle

---
+++ src/debounce/debounce.js
@@ -0,0 +1,19 @@
+/**
+ * 防抖函数
+ * @param method 事件触发的操作
+ * @param delay 多少毫秒内连续触发事件，不会执行
+ * @returns {Function}
+ */
+function debounce(method,delay) {
+    let timer = null;
+    return function () {
+        let self = this,
+            args = arguments;
+        timer && clearTimeout(timer);
+        timer = setTimeout(function () {
+            method.apply(self,args);
+        },delay);
+    }
+}
+
+module.exports = debounce;
